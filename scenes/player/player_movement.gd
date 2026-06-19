94fd6cd82b5aca698b79e0f08f26640189498662
Jose Burgos <joseburgosmtnz@gmail.com>
2026-06-19 18:43:53 +0200

    movimiento bien hecho

--- scenes/player/player_movement.gd
+++ scenes/player/player_movement.gd
@@ -5,13 +5,12 @@ extends Node2D
 @export var papa: CharacterBody2D;
 
 var target = position
-
-func _input(event):
 	# Use is_action_pressed to only accept single taps as input instead of mouse drags.
-	if event.is_action_pressed("click"):
-		target = get_global_mouse_position()
+	
 
-func _physics_process(delta):
+func _physics_process(_delta):
+	if Input.is_action_pressed("click"):
+		target = get_global_mouse_position()
 	papa.velocity = papa.position.direction_to(target) * speed
 	# look_at(target)
 	if papa.position.distance_to(target) > 10:
