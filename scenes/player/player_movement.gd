585bd90172aec90708d84f57ea817e24dec3bf9f
Jose Burgos <joseburgosmtnz@gmail.com>
2026-06-19 18:34:34 +0200

    movimiento ratón

--- scenes/player/player_movement.gd
+++ scenes/player/player_movement.gd
@@ -4,12 +4,15 @@ extends Node2D
 
 @export var papa: CharacterBody2D;
 
-func get_input():
-	look_at(get_global_mouse_position())
-	papa.velocity = transform.x * Input.get_axis("forward", "backwards") * speed
-	print('input')
-	print(papa)
-func _physics_process(_delta):
-	get_input()
-	papa.move_and_slide()
-	print(papa.velocity)
+var target = position
+
+func _input(event):
+	# Use is_action_pressed to only accept single taps as input instead of mouse drags.
+	if event.is_action_pressed("click"):
+		target = get_global_mouse_position()
+
+func _physics_process(delta):
+	papa.velocity = papa.position.direction_to(target) * speed
+	# look_at(target)
+	if papa.position.distance_to(target) > 10:
+		papa.move_and_slide()
