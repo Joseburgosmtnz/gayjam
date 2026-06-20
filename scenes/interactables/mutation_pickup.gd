73c30ed05a4c087163ab64c86577e26f5315daf5
Adri <adrian.sp.6@gmail.com>
2026-06-20 19:36:07 +0200

    Añadir lógica de mutaciones y actualizar referencias en escenas interactivas

--- scenes/interactables/mutation_pickup.gd
+++ scenes/interactables/mutation_pickup.gd
@@ -3,12 +3,16 @@ extends Interactable
 class_name MutationPickUp
 
 #Asignamos una mutacion ya creada
-@export var mutation : Mutation
+@export var mutation_resource : Mutation
+var mutation_scene := preload("res://scenes/mutations/mutation.tscn")
 
 func _on_interacted(player : Node2D) -> void:
 	# Comprobamos que el jugador posea la mutacion counter
 	# si la tiene ejecutamos la animacion de destruccion
 	print("El jugador ha interaccionado con el pickup")
 	print("El jugador adquiere la mutación")
-	player.mutations.append(mutation)
+	player.mutations.append(mutation_resource)
+	
+	player.add_child(mutation_scene.instantiate())
+	
 	_play_interaction_animation()
