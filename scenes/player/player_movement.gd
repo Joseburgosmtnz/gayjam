05a2b9fd4656ca9636ca03079f3608153c899bdb
Jose Burgos <joseburgosmtnz@gmail.com>
2026-06-21 12:32:33 +0200

    No consigo cambiar la posición del eje Z del personaje en los créditos

--- scenes/player/player_movement.gd
+++ scenes/player/player_movement.gd
@@ -9,9 +9,10 @@ var target = position
 	
 
 func _physics_process(_delta):
-	if Input.is_action_pressed("click"):
+	if Input.is_action_pressed("click") and get_tree().current_scene.name != "Creditos":
 		target = get_global_mouse_position()
 	papa.velocity = papa.position.direction_to(target) * speed
 	# look_at(target)
-	if papa.position.distance_to(target) > 10:
+	if papa.position.distance_to(target) > 10 and get_tree().current_scene.name != "Creditos":
 		papa.move_and_slide()
+	
